f3a729d618d4bb936ba7f2ec88b0e40fb0efedd4
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:13:27 +0000

    [automatic commit] 2025-06-20 21:13:27 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -55,7 +55,7 @@ let db;
 app.get('/', async (req, res) => {
   try {
     const [dogs] = await db.execute(`
-            SELECT
+      SELECT
         d.name AS dog_name,
         d.size,
         u.username AS owner_username
