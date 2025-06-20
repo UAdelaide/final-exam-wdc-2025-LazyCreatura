85ec20f8c4d891c2eb1be1ac4fc2dd816622c35c
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 20:34:39 +0000

    [automatic commit] 2025-06-20 20:34:39 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -28,10 +28,15 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+
+      INSERT INTO Dogs (owner_id, name, size)
+
+      VALUES
+      ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+      ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small' ),
+      ((SELECT user_id FROM Users WHERE username = 'bobwalker'), 'Chicharon', 'large'),
+      ((SELECT user_id FROM Users WHERE username = 'Hye'), 'Jolibee', 'large'),
+      ((SELECT user_id FROM Users WHERE username = 'wanwan'), 'BBQ', 'small');
       `);
     }
   } catch (err) {
