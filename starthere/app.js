0f70b4cb9a54ed9415e27cd47ecc683e5db0c02c
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:51:34 +0000

    [automatic commit] 2025-06-20 21:51:34 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -60,7 +60,7 @@ let db;
       INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
       VALUES
       (1, 2, 1, 4, 'Mad Max'),
-      (2, 5, 3, 5, 'Excellent service for Bella!'),
+      (2, 5, 3, 5, 'Cute puppy as well as her name'),
       (3, 2, 4, 3, 'Good effort with Rocky.');
       `);
 
