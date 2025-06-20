4b21e9a870d87f6939ca774264f91cde6538770b
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 20:39:40 +0000

    [automatic commit] 2025-06-20 20:39:40 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -54,10 +54,10 @@ let db;
 // Route to return dogs as JSON
 app.get('/', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [books] = await db.execute('SELECT * FROM Dogs');
     res.json(books);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs list' });
   }
 });
 
