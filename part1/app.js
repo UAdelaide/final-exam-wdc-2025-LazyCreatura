a14180fe34979077b4217087cf91d1b75972e9b3
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 20:36:40 +0000

    [automatic commit] 2025-06-20 20:36:40 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -22,15 +22,19 @@ let db;
       password: '',
       database: 'DogWalkService'
     });
-    // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM U');
+    // Insert user if table is empty
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+      INSERT INTO Users (username, email, password_hash, role)
+      VALUES
+      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+      ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+      ('Hye', 'miniHye@example.com', 'hashed777', 'owner'),
+      ('wanwan', 'Wank@example.com', 'hashed777', 'walker');
       `);
+
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
