17c29db0c5e554adf09d1dd5035499b3aa05d1bf
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 20:52:03 +0000

    [automatic commit] 2025-06-20 20:52:03 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -55,7 +55,10 @@ let db;
 app.get('/', async (req, res) => {
   try {
     const [dogs] = await db.execute('SELECT * FROM Dogs');
-    await db.execute()
+    await db.execute(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id`);
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs list' });
