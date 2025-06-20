eaf9561e063118dfb3bb17a9716e66d6ecbf80f8
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:33:51 +0000

    [automatic commit] 2025-06-20 21:33:51 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -59,7 +59,7 @@ let db;
       await db.execute(`
       INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments, rated_at)
       VALUES
-        ((SELECT request_id FROM WalkRequests WHERE status = 'accepted' LIMIT 1), (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'alice123'), 4),
+        ((SELECT request_id FROM WalkRequests WHERE status = 'accepted' LIMIT 1), (SELECT user_id FROM Users WHERE username = 'bobwalker'), (SELECT user_id FROM Users WHERE username = 'alice123'), 4, 'mid'),
         ((SELECT request_id FROM WalkRequests WHERE status = 'accepted' LIMIT 1 OFFSET 1), (SELECT user_id FROM Users WHERE username = 'emilywalker'), (SELECT user_id FROM Users WHERE username = 'carol123'), 5);
       `);
       `);
