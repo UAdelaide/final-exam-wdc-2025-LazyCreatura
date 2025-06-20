b989320f5ea0e87d68920f9bfb330d27f64c4d50
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:13:07 +0000

    [automatic commit] 2025-06-20 21:13:07 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -54,7 +54,14 @@ let db;
 // Route to return dogs as JSON
 app.get('/', async (req, res) => {
   try {
-    const [dogs] = await db.execute('SELECT  FROM Dogs');
+    const [dogs] = await db.execute(`
+            SELECT
+        d.name AS dog_name,
+        d.size,
+        u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id`
+    );
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(dogs, null, 2));
   } catch (err) {
