9644660ede3da163e01c361c97418fa89cd97612
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:11:47 +0000

    [automatic commit] 2025-06-20 21:11:47 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -56,7 +56,7 @@ app.get('/', async (req, res) => {
   try {
     const [dogs] = await db.execute('SELECT * FROM Dogs');
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(dogs, null, 2)); 
+    res.send(JSON.stringify(dogs, null, 2));
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs list' });
   }
