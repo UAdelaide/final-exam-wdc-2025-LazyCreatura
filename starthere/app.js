29a2a3b47a30fa110800cfe9eef097492b167ea9
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 20:54:44 +0000

    [automatic commit] 2025-06-20 20:54:44 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -52,7 +52,7 @@ let db;
 })();
 
 // Route to return dogs as JSON
-app.get('/api/dogs', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const [rows] = await db.execute(`
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
