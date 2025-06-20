4b5ae4a2c90b5d557e8e86c493bcc4e95ed1cd33
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 20:12:56 +0000

    [automatic commit] 2025-06-20 20:12:56 UTC 20.227.140.178

--- part1/app.js
+++ part1/app.js
@@ -14,19 +14,8 @@ app.use(cookieParser());
 let db;
 
 (async () => {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: '127.0.0.1',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
-
-    // Now connect to the created database
+    //connect to the created database
     db = await mysql.createConnection({
       host: '127.0.0.1',
       user: 'root',
