51af4885a42e68dea93c98346c060f605db90e13
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:05:26 +0000

    [automatic commit] 2025-06-20 21:05:26 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -52,31 +52,15 @@ let db;
 })();
 
 // Route to return dogs as JSON
-// app.get('/', async (req, res) => {
-//   try {
-//     const [dogs] = await db.execute('SELECT * FROM Dogs');
-//     res.json(dogs);
-//   } catch (err) {
-//     res.status(500).json({ error: 'Failed to fetch dogs list' });
-//   }
-// });
-// Route to return dogs as JSON
 app.get('/', async (req, res) => {
   try {
-    const [dogs] = await db.execute(`
-      SELECT
-        d.name AS dog_name,
-        d.size,
-        u.username AS owner_username
-      FROM Dogs d
-      JOIN Users u ON d.owner_id = u.user_id
-    `);
+    const [dogs] = await db.execute('SELECT * FROM Dogs');
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs list' });
   }
-  
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
