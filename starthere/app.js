eb9542dd2982a9cd09ec4dd7335c54f573d93a54
Hye <117409223+LazyCreatura@users.noreply.github.com>
2025-06-20 21:08:06 +0000

    [automatic commit] 2025-06-20 21:08:06 UTC 20.227.140.178

--- starthere/app.js
+++ starthere/app.js
@@ -63,6 +63,8 @@ app.get('/', async (req, res) => {
       FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
     `);
+    res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(dogs, null, 2)); // Pretty-print JSON with 2-space indent
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs list' });
